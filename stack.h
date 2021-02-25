bf15fb374f44a771d1d0ed1c2b6e7d4f9dcdede3
HunterPlus <zlw08140957@gmail.com>
2021-02-25 19:19:15 +0800

    Update stack.h

--- stack.h
+++ stack.h
@@ -10,32 +10,3 @@ struct stack *st_create(int size);
 void st_push(struct stack *st, char c);
 char st_pop(struct stack *st);
 
-void st_push(struct stack *st, char c)
-{
-  if(st->top == st->size-1) {
-    printf("stack is full, could not push!");
-    exit(1);
-  }
-  st->data[++st->top] = c;
-}
-
-struct stack *st_create(int size)
-{
-  struct stack *st;
-  st = (struct stack *)malloc(sizeof(struct stack));
-  st->size = size;
-  st->top = -1;
-  st->data = (char *)malloc(size);
-  
-  return st;
-}
-char st_pop(struct stack *st)
-{
-  char c;
-  if(st->top == -1) {
-    printf("stack is empty, could not pop!");
-    exit(1);
-  }
-  c = st->data[st->top--];
-  return c;
-}
